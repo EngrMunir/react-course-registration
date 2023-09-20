6cebac8d03b0bcacf983a29326906d687fdb4209
Md Sirajul Munir <abc@gmail.com>
2023-09-20 19:04:01 +0600

    blogs and bookmarks components ready

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
+
+function App() {
+  return (
+    <>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bookmarks = () => {
+    return (
+        <div>
+            <h2>Bookmark section</h2>
+        </div>
+    );
+};
+
+export default Bookmarks;
